2a673e67e27b915a4830e7e950f2d4444d24efc4
Devin Lane <devin.c.lane@gmail.com>
2025-06-26 09:01:34 -0700

    add initial frontend

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import ImageWithInfo from "./components/image-with-info";
+
+type Image = {
+    id: string;
+    author: string;
+    download_url: string;
+    width: number;
+    height: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [images, setImages] = useState<Image[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        async function fetchImages() {
+            try {
+                const url = "http://localhost:3000/api/images";
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                const data = await response.json();
+                setImages(data);
+            } catch (error) {
+                if (error instanceof Error) {
+                    setError(error.message);
+                    console.error(error.message);
+                } else {
+                    setError(String(error));
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchImages();
+    }, []);
+    if (error) return <p>Error fetching images: {error}</p>;
+    if (isLoading) return <p>Loading...</p>;
+    // for each image, show the original, then a banner, and a thumbnail
+    return (
+        <div>
+            <h1>Optimized images</h1>
+
+            {images.map((img) => (
+                <div key={img.id}>
+                    {/* original image */}
+                    <ImageWithInfo
+                        src={`http://localhost:3000/optimize/w_600/${encodeURIComponent(
+                            img.download_url
+                        )}`}
+                    />
+
+                    {/* bannner */}
+                    <ImageWithInfo
+                        src={`http://localhost:3000/optimize/s_400x100,format_webp/${encodeURIComponent(
+                            img.download_url
+                        )}`}
+                    />
+                    {/* thumbnail */}
+                    <ImageWithInfo
+                        src={`http://localhost:3000/optimize/s_100x100,format_webp/${encodeURIComponent(
+                            img.download_url
+                        )}`}
+                    />
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default App
+export default App;
